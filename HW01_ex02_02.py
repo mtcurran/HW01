55047bf1920e8b983192d6ed38921fb750c13018
Max Curran <mtcurran@berkeley.ischool.edu>
2015-08-04 23:32:02 -0700

    Submission of Day 1 Homework

--- HW01_ex02_02.py
+++ HW01_ex02_02.py
@@ -9,25 +9,26 @@
 # and the type (of the value of the expression).
 
 # 1. width/2
-# value:
-# type:
+# value: 2
+# type: int
 
 # 2. width/2.0
-# value:
-# type:
+# value: 8.5
+# type: float
 
 # 3. height/3
-# value:
-# type:
+# value: 4.0
+# type: float
 
 # 4. 1 + 2 * 5
-# value:
-# type:
+# value: 11
+# type: int
 
 # 5. delimiter * 5
-# value:
-# type:
+# value: '.....'
+# type: str
 # 
 # Use the Python interpreter to check your answers.
 # [comment on any surprises here]
+# I had no idea you could multiply strings!!
 # 
